2117ef3b8f973e97ff546b11d56350e02252ca3c
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-08 23:50:54 -0400

    Update load_transform.py

--- mylib /load_transform.py
+++ mylib /load_transform.py
@@ -22,14 +22,10 @@ def load(dataset1='data/commodity-prices.csv'):
     conn_str = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
     with pyodbc.connect(conn_str) as conn:
         cursor = conn.cursor()
-        cursor.execute("DROP TABLE IF EXISTS laliga")
-        cursor.execute("CREATE TABLE laliga (Date varchar(255), HomeTeam varchar(255), AwayTeam varchar(255), home_goals int, away_goals int, result varchar(255), home_goals_half_time int, away_goals_half_time int, result_half_time varchar(255), home_shots int, away_shots int, home_shots_on_target int, away_shots_on_target int, home_fouls int, away_fouls int, home_corners int, away_corners int, home_yellow_cards int, away_yellow_cards int, home_red_cards int, away_red_cards int)")
+        cursor.execute("DROP TABLE IF EXISTS commodity-prices")
+        cursor.execute("CREATE TABLE commodity-prices (Date,All Commodity Price Index,Non-Fuel Price Index,Food and Beverage Price Index,Food Price Index,Beverage Price Index,Industrial Inputs Price Index)")
         for _, row in df1.iterrows():
-            cursor.execute("INSERT INTO laliga VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", row.tolist())
-        cursor.execute("DROP TABLE IF EXISTS epl")
-        cursor.execute("CREATE TABLE epl (Date varchar(255), HomeTeam varchar(255), AwayTeam varchar(255), home_goals int, away_goals int, result varchar(255), home_goals_half_time int, away_goals_half_time int, result_half_time varchar(255), home_shots int, away_shots int, home_shots_on_target int, away_shots_on_target int, home_fouls int, away_fouls int, home_corners int, away_corners int, home_yellow_cards int, away_yellow_cards int, home_red_cards int, away_red_cards int)")
-        for _, row in df2.iterrows():
-            cursor.execute("INSERT INTO epl VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", row.tolist())
+            cursor.execute("INSERT INTO laliga VALUES (?,?,?,?,?,?,?)", row.tolist())
         
         cursor.close()
 
